65b4d3d6508700c20cc2beac946d2b1b3b7106e1
dungtran3864 <zungmr@gmail.com>
2023-06-01 22:54:00 -0400

    Cache max rooms in storage

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -3,30 +3,50 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { getMultipleDocuments, getSingleDocument } from "@/firebase/utils";
-import { GAMES_PATH, LOBBY_STATE, USERS_PATH } from "@/constants";
+import {
+  GAMES_PATH,
+  LOBBY_STATE,
+  MAX_ROOMS_STORAGE_KEY,
+  USERS_PATH,
+} from "@/constants";
 import { where } from "firebase/firestore";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function GamePinScreen() {
   const [pin, setPin] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const router = useRouter();
+  const [maxRooms, setMaxRooms] = useLocalStorage(MAX_ROOMS_STORAGE_KEY, {});
+
+  function isMaxRoomAlreadyChecked() {
+    return maxRooms && maxRooms[pin.trim()];
+  }
 
   async function joinRoom(e) {
     e.preventDefault();
     const gameData = await getSingleDocument(GAMES_PATH, pin.trim());
     if (gameData) {
       if (gameData.state === LOBBY_STATE) {
+        if (isMaxRoomAlreadyChecked()) {
+          setErrorMessage(
+            "This game is already at max capacity. Please join another game."
+          );
+          return;
+        }
         const [numberOfPlayers] = await getMultipleDocuments(
           USERS_PATH,
           where("roomId", "==", pin.trim())
         );
-        if (numberOfPlayers < 100) {
+        if (numberOfPlayers < 2) {
           router.push(`/join/${pin}?isHost=false`);
-        } else {
-          setErrorMessage(
-            "This game is already at max capacity. Please join another game."
-          );
+          return;
         }
+        const copyMaxRooms = {...maxRooms};
+        copyMaxRooms[pin.trim()] = true;
+        setMaxRooms(copyMaxRooms);
+        setErrorMessage(
+          "This game is already at max capacity. Please join another game."
+        );
       } else {
         setErrorMessage(
           "This game has already started. Please join another game."
